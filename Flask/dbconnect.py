a16a64d7a155fc16e602dd614af96e975aa42ff7
Bhavya Chugh <chugh.bhavya@gmail.com>
2017-06-03 20:19:19 -0400

    Update dbconnect.py

--- Flask/dbconnect.py
+++ Flask/dbconnect.py
@@ -1,7 +1,11 @@
+# Author : Bhavya Chugh
+# Created on : 05/20/2017
+
 import mysql.connector
 
+# Making a connection with database and returning cursor and connection object
 def connection():
-    conn = mysql.connector.connect(user = "root", password = "bhavyaroot", host = "localhost"
+    conn = mysql.connector.connect(user = "root", password = "******", host = "localhost"
                        ,database = "students")
     mycursor = conn.cursor()
 
