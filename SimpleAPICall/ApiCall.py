6a8087a93428a38fb63651d160dcad427547aaee
chughbhavya <chugh.bhavya@gmail.com>
2018-04-06 11:40:52 -0500

    Changed the tenperature to Farenheit

--- SimpleAPICall/ApiCall.py
+++ SimpleAPICall/ApiCall.py
@@ -15,7 +15,8 @@ def temperature():
     r = requests.get('http://samples.openweathermap.org/data/2.5/weather?zip=+zipcode+,us&appid=3a6eb028f243c17e534c9b4cdb7da188')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
-    return render_template('temperature.html', temp = str(temp_k))
+    temp_f = 1.8 * (temp_k - 273 ) + 32
+    return render_template('temperature.html', temp = str(temp_f))
 
 @app.route('/', methods = ['GET','POST'])
 
